938644c9abaa1c3a977746ce11b0bcc3ab9238a9
Nam Cao <namcao@linutronix.de>
2024-05-08 07:43:00 +0200

    remove fzf

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ cd ~/.vim/pack/nam/start
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/tomasiser/vim-code-dark
 git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/junegunn/fzf.vim.git
 git clone https://github.com/preservim/nerdtree.git
 git clone https://github.com/vim-airline/vim-airline.git
 
