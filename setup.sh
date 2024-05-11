96ae59b0714c69e28cf57595af22fe1b5e47bd46
Nam Cao <namcao@linutronix.de>
2024-05-11 07:45:44 +0200

    go back to VSCode color theme
    This reverts commit fc7d0bb4f354c6066b22758f0fd8d2c78ccb2b8f.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.vim/pack/nam/start
 cd ~/.vim/pack/nam/start
 rm -rfd *
 git clone --depth=1 https://github.com/tpope/vim-fugitive.git
-git clone --depth=1 https://github.com/morhetz/gruvbox.git
+git clone --depth=1 https://github.com/tomasiser/vim-code-dark
 git clone --depth=1 https://github.com/airblade/vim-gitgutter.git
 git clone --depth=1 https://github.com/preservim/nerdtree.git
 
