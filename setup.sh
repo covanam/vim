ddfe2137592c429824ee3690bb10f1c208a8071a
Nam Cao <namcao@linutronix.de>
2024-05-11 06:48:25 +0200

    refresh everything in setup

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ cp $(dirname $0)/vimrc ~/.vimrc
 
 mkdir -p ~/.vim/pack/nam/start
 cd ~/.vim/pack/nam/start
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/morhetz/gruvbox.git
-git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/preservim/nerdtree.git
-git clone https://github.com/vim-airline/vim-airline.git
+rm -rfd *
+git clone --depth=1 https://github.com/tpope/vim-fugitive.git
+git clone --depth=1 https://github.com/morhetz/gruvbox.git
+git clone --depth=1 https://github.com/airblade/vim-gitgutter.git
+git clone --depth=1 https://github.com/preservim/nerdtree.git
+git clone --depth=1 https://github.com/vim-airline/vim-airline.git
 
