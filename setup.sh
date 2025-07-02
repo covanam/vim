68c8a8629a77b2a892e49e16716e61860206dc19
Nam Cao <namcao@linutronix.de>
2025-07-02 12:29:18 +0200

    Revert "Remove easymotion"
    This reverts commit 0c3b081f5392064d7ea19870958173608225aeb6.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cd ~/.vim/pack/nam/start
 rm -rfd *
 git clone --depth=1 https://github.com/tpope/vim-fugitive.git
 git clone --depth=1 https://github.com/airblade/vim-gitgutter.git
+git clone --depth=1 https://github.com/easymotion/vim-easymotion.git
 git clone --depth=1 https://github.com/morhetz/gruvbox.git
 git clone --depth=1 https://github.com/tpope/vim-sleuth.git
 git clone --depth=1 https://github.com/ntpeters/vim-better-whitespace.git
